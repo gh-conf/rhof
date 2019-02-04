e05a01904d0e31eb0efd88ff0b265091cc4007b4
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-02-04 23:25:52 +0530

    unused value removed

--- tests/test.js
+++ tests/test.js
@@ -2,8 +2,6 @@ const expect = require('chai').expect;
 
 const rhof = require('../index');
 
-const CONTRIBUTOR_ARSHAD_HOF = '<a href="https://github.com/';
-
 
 describe('Should generate the required hof', () => {
   it('should have arshad hof as start value for gh-conf/rhof repo', async () => {
